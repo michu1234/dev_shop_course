40569496b0c0ccf040c8bc293e6f453cd5072a48
unknown <dawiddeveloper@gmail.com>
2018-12-30 23:52:55 +0100

    markup changes

--- dist/js.5c615dc2.js
+++ dist/js.5c615dc2.js
@@ -4548,7 +4548,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55117" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53247" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
